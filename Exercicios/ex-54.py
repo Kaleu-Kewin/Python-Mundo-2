07875ea4ad4af184f0409f5e67dfe3cecfb91653
Kaléu <kaleu.depaula9@gmail.com>
2025-04-27 17:58:15 -0300

    commit: Exercicio 54 - Ajuste

--- Exercicios/ex-54.py
+++ Exercicios/ex-54.py
@@ -2,14 +2,18 @@
 
 from datetime import date
 
-maioridade = 0
-ano_atual  = date.today().year
+maior = 0
+menor = 0
+
+ano_atual = date.today().year
 
 for i in range(0, 7):
-    nasc = int(input('Digite seu ano de nascimento: '))
+    nasc  = int(input('Digite seu ano de nascimento: '))
     idade = ano_atual - nasc
     
     if idade >= 18:
-        maioridade = maioridade + 1
+        maior += 1
+    else:
+        menor += 1
         
-print(f'{maioridade} pessoas são maior de idade')
+print(f'{maior} pessoas são maior de idade e {menor} são menor de idade')
